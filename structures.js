cee1dddbc5a879d02223f58541194da37aed9ba3
LoicBrassart <LoicBrassart@users.noreply.github.com>
2021-10-05 14:16:15 +0200

    First batch

---
+++ structures.js
@@ -0,0 +1,57 @@
+// ---------------------------------------------------------------------------------------- Explications
+// - Réalise un maximum d'exercices dans le temps imparti
+// - Tip: Active ou désactive tes réponses en changeant les valeurs dans les conditions
+// - On te donne accès aux fonctions suivantes pour tout ce dojo/kata.
+//   /!\ Il t'est **interdit** de toucher à ce code !
+
+// Definir un nombre aléatoire entre 0 et 1
+const random = ()=>{
+    return Math.random()
+}
+
+// ---------------------------------------------------------------------------------------- Exercices
+// Ecrire une variable contenant ton age (number) et une constante contenant ta date de naisance (string)
+if(false) {
+
+}
+
+// Ecrire un code qui t'affiche ton prénom si ton âge est pair (... Quoi ? Pourquoi pas ?!)
+if(false) {
+    
+}
+
+// Ecrire un code qui t'affiche ton prénom si tu as moins de 27 ans, et ton nom dans le cas contraire
+if(false) {
+    
+}
+
+// Ecrire un code qui me copie 10 lignes de "Je ne copierai pas du code bêtement" (oui, c'est du recyclage d'exos :p)
+if(false) {
+    
+}
+
+// Ecrire une fonction getToto qui me renvoie la string "toto"
+if(false) {
+    
+}
+
+// Ecrire une fonction getTotoArrow qui me renvoie la string "toto", en utilisant la syntaxe fléchée
+if(false) {
+    
+}
+
+// Ecrire une fonction d6 qui me renvoie un nombre alétoire entre 1 et 6
+if(false) {
+    
+}
+
+// Ecrire une fonction qui me renvoie la somme de deux nombres passés en paramètres
+if(false) {
+    
+}
+
+// Ecris un objet littéral (sans POO) qui te représente: nom, age, sexe, loisirs, ton langage de prédilaection, une fonction pour te présenter, une fonction qui récite ton elevator speech
+if(false) {
+    
+}
+
